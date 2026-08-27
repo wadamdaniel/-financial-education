1a38fab55bd40aa6bcea2be2a86bc6e2e63d6973
wadamdaniel <wadamdaniel@gmail.com>
2026-08-27 17:37:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const C="fe-v59";
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","index.html","manifest.json"]))));
+const C="fe-clean-v10";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","index.html","manifest.json"])))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
-self.addEventListener("message",e=>{if(e.data&&e.data.type==="SKIP_WAITING")self.skipWaiting()});
-self.addEventListener("fetch",e=>{if(e.request.mode==="navigate"){e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("index.html"))));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener("fetch",e=>{if(e.request.mode==="navigate"){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("index.html"))));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
